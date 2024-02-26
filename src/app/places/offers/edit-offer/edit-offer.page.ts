4bff952141c336a51e14559e8017c7358c30abd4
elchinovzla <sonnyng.vzla@gmail.com>
2024-02-25 17:18:42 -0700

    add navigator menu

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Place } from 'src/app/places/place.mode';
+import { PlacesService } from 'src/app/places/places.service';
 
 @Component({
   selector: 'app-edit-offer',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-offer.page.scss'],
 })
 export class EditOfferPage implements OnInit {
+  place: Place | undefined;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private placesService: PlacesService,
+    private navCtrl: NavController
+  ) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/offers');
+        return;
+      }
+      //@ts-ignore
+      this.place = this.placesService.getPlace(paramMap.get('placeId')!);
+    });
   }
-
 }
--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Place } from 'src/app/places/place.mode';
+import { PlacesService } from 'src/app/places/places.service';
 
 @Component({
   selector: 'app-offers',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./offers.page.scss'],
 })
 export class OffersPage implements OnInit {
+  offers: Place[] = [];
 
-  constructor() { }
+  constructor(private placesService: PlacesService) {}
 
   ngOnInit() {
+    this.offers = this.placesService.getPlaces();
   }
-
 }
